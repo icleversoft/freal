ba594937a84d3926ddf1b10d2e8ea13c22ff02ff
icleversoft <iphone@icleversoft.com>
2013-09-21 21:25:23 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freal::Application.config.secret_token = '69ce561f557b851af0035e39a030c0390cdfa71c48b7f7f0ea191e5a6fecd1bb6ab54378f3400abbf9d3279189a296be0900151ae6230c8b218ef26151ca126a'
