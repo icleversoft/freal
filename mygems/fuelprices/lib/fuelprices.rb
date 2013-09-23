242b2ebb9ebca8d6930dc75b40a51c20a25ef814
icleversoft <iphone@icleversoft.com>
2013-09-24 00:15:48 +0300

    fuelprices gem

--- mygems/fuelprices/lib/fuelprices.rb
+++ mygems/fuelprices/lib/fuelprices.rb
@@ -1,5 +1,11 @@
 require "fuelprices/version"
-
+#d=Nokogiri::HTML(open('http://www.fuelprices.gr/CheckPrices?DD=A1140100&prodclass=1'))
+#entries = d.search("td[@class='mainArea']/table")
+# if entries.size > 2
+#   2.times do
+#     entries.delete(entries.first)
+#   end
+# end
 module Fuelprices
   # Your code goes here...
 end
