3622c43b4fd2eea5faa9edea5b6177317b3c972b
iCleversoft <iphone@icleversoft.com>
2014-03-02 17:29:15 +0200

    index view fixed for city controller

--- app/controllers/admin/cities_controller.rb
+++ app/controllers/admin/cities_controller.rb
@@ -1,4 +1,9 @@
 class Admin::CitiesController < Admin::ManageController
   def index
+    @page = params[:page]
+    @page ||= 1
+    @page = @page.to_i
+    
+    @cities = City.all.asc(:name).page(@page)
   end
 end
