dab0002b7f8e8e8a168a7a3efebccfc1bef28b38
iCleversoft <iphone@icleversoft.com>
2014-03-30 20:20:51 +0300

    render a notification message when none city found for specified location

--- app/controllers/api/v1/cities_controller.rb
+++ app/controllers/api/v1/cities_controller.rb
@@ -2,6 +2,10 @@ class Api::V1::CitiesController < Api::V1::BaseController
   def index
     location = [params[:lat], params[:lon]].collect{|i| i.to_f}
     city = City.get_closest_cities(location).first
-    respond_with(:id => city._id, :name => city.name, :location => location, :stations => city.city_municipality)
+    if city.nil?
+      respond_with(message: 'Not found any city'), status: 400
+    else
+      respond_with(:id => city._id, :name => city.name, :location => location, :stations => city.city_municipality), status: 200
+    end
   end
 end
