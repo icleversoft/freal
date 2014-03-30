2bf370ea7da29103e36a64099db70d58e261c546
iCleversoft <iphone@icleversoft.com>
2014-03-30 20:22:15 +0300

    fixes

--- app/controllers/api/v1/cities_controller.rb
+++ app/controllers/api/v1/cities_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::CitiesController < Api::V1::BaseController
     location = [params[:lat], params[:lon]].collect{|i| i.to_f}
     city = City.get_closest_cities(location).first
     if city.nil?
-      respond_with(message: 'Not found any city'), status: 400
+      respond_with(message: 'Not found any city', status: 400)
     else
-      respond_with(:id => city._id, :name => city.name, :location => location, :stations => city.city_municipality), status: 200
+      respond_with(:id => city._id, :name => city.name, :location => location, :stations => city.city_municipality, status: 200)
     end
   end
 end
