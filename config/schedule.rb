df1e662b0de43a8a9d4faa0e3539d901fc0f376e
iCleversoft <iphone@icleversoft.com>
2014-04-16 21:10:29 +0300

    schedule removed

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,8 +18,8 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
-every 2.hours do
-  # runner "MyModel.some_process"
-  rake "updater:price"#, :output => {:error => 'error.log', :standard => 'cron.log'}
-  # command "/usr/bin/my_great_command"
-end
+# every 2.hours do
+#   # runner "MyModel.some_process"
+#   rake "updater:price"#, :output => {:error => 'error.log', :standard => 'cron.log'}
+#   # command "/usr/bin/my_great_command"
+# end
