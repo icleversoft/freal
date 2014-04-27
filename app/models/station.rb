c2d5507d78ec1ea1ceb6cd7be2c494e7070bb30e
iCleversoft <iphone@icleversoft.com>
2014-04-27 20:40:11 +0300

    trying to fix apn problem

--- app/models/station.rb
+++ app/models/station.rb
@@ -96,7 +96,7 @@ class Station
       favorites.each do |fav|
         notify.device_token = fav.device.token
         # notify.alert = "#{price_value} / #{submitted}"
-        notify.alert = "#{price_value}E"
+        notify.alert = "#{address}, #{price_value}E"
         notify.badge = 0
         notify.sound = "default.aiff"
         notify.push  
